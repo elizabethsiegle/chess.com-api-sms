b62dca931a55f42e655234381f275a62071741d1
Lizzie Siegle <esiegle@brynmawr.edu>
2021-01-14 15:50:00 -0800

    first commit from twitch

---
+++ functions/chess-open-to-other-usernames.js
@@ -0,0 +1,28 @@
+const got = require('got');
+exports.handler = async function(context, event, callback) {
+  const twiml = new Twilio.twiml.MessagingResponse();
+  const username = event.Body.toLowerCase().trim();
+  const stats_url = `https://api.chess.com/pub/player/${username}/stats`;
+  const stats_resp = await got(stats_url);
+  const data = JSON.parse(stats_resp.body);
+  const chess_rapid = data.chess_rapid;
+  if (typeof chess_rapid == "undefined") {
+      twiml.message('that is not a user on chess.com');
+      callback(null, twiml);
+    }
+    else {
+        const rapid_record = chess_rapid.record;
+        const win = rapid_record.win;
+        const loss = rapid_record.loss;
+        const draw = rapid_record.draw;
+        const rapid_best = chess_rapid.best;
+        const rating = rapid_best.rating;
+        const unix_timestamp = rapid_best.date;
+        var date = new Date(unix_timestamp * 1000);
+        date =date.toLocaleString(); // outputs to "Thu May 28 2015"
+        twiml.message(`${username}'s rapid record: ${win} wins, ${loss} losses, ${draw} draws. Their rapid_best rating: ${rating} from ${date}`);
+        
+        callback(null, twiml);
+    }
+  
+};
