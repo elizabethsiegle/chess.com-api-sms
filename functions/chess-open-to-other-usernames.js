5987b55e312b5d4efa6bbb80015b316206ccc8f7
Lizzie Siegle <esiegle@brynmawr.edu>
2021-01-19 22:04:45 -0800

    PROFILE PIC SENT

--- functions/chess-open-to-other-usernames.js
+++ functions/chess-open-to-other-usernames.js
@@ -1,9 +1,13 @@
 const got = require('got');
 exports.handler = async function(context, event, callback) {
   const twiml = new Twilio.twiml.MessagingResponse();
+  const message = twiml.message(); 
   const username = event.Body.toLowerCase().trim();
   const stats_url = `https://api.chess.com/pub/player/${username}/stats`;
+  const profile_url = `https://api.chess.com/pub/player/${username}`
   const stats_resp = await got(stats_url);
+  const profile_resp = await got(profile_url);
+  const prof_data = JSON.parse(profile_resp.body);
   const data = JSON.parse(stats_resp.body);
   const chess_rapid = data.chess_rapid;
   if (typeof chess_rapid == "undefined") {
@@ -11,6 +15,7 @@ exports.handler = async function(context, event, callback) {
       callback(null, twiml);
     }
     else {
+      const avatar_url = prof_data.avatar;
       const rapid_record = chess_rapid.record;
       const win = rapid_record.win;
       const loss = rapid_record.loss;
@@ -21,8 +26,8 @@ exports.handler = async function(context, event, callback) {
       const unix_timestamp = rapid_best.date;
       var date = new Date(unix_timestamp * 1000);
       date = date.toDateString(); // outputs to "Thu May 28 2015 format"
-      twiml.message(`${username}'s rapid record: ${win} wins, ${loss} losses, ${draw} draws. Current rapid rating: ${rapid_last}, best rapid rating: ${rapid_best_rating} from ${date}`);
+      message.body(`${username}'s rapid record: ${win} wins, ${loss} losses, ${draw} draws. Current rapid rating: ${rapid_last}, best rapid rating: ${rapid_best_rating} from ${date}`);
+      message.media(avatar_url);
       callback(null, twiml);
     }
-  
 };
